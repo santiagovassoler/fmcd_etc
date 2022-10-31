a1315516733d5dd778ae811f53adf220e7ab3a3a
santiago <santiagovassoler@gmail.com>
2022-10-31 14:13:56 +0000

    run mix format

--- lib/fmcd_etc/segment_helper.ex
+++ lib/fmcd_etc/segment_helper.ex
@@ -1,4 +1,9 @@
 defmodule FmcdEtc.SegmentHelper do
+  @moduledoc """
+    A helper module to build common things around segments
+    TODO: Probably will be good to create the whole segment map from here
+    since they all share the same keys.
+  """
   def format_date(date, time) do
     {:ok, date_time, _} = DateTime.from_iso8601(date <> " " <> time <> ":00Z")
     date_time
